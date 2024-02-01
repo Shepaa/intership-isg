aee5984102b36c89118f4c3ac6b6d20e1624e30f
Ivan <shepelevivan11032006@gmail.com>
2024-02-02 01:15:00 +0200

    add gitHubApiTask

---
+++ gitHubApiTask/01-gitAPI/index.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('#getWaitersBtn');
+const list = document.querySelector('#list');
+const inputEl = document.querySelector('#input');
+const loader = document.querySelector('#loader');
+
+btn.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+    loader.style.display = 'block';
+    fetch(`https://api.github.com/users/${inputEl.value}/repos`)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then((respond) => {
+            loader.style.display = 'none';
+            respond.forEach((repo) => {
+                renderData(repo);
+            });
+        })
+        .catch((error) => {
+            loader.style.display = 'none';
+            alert(error)
+        });
+}
+
+function renderData(data) {
+    const HTML = `
+        <div class="repository-card">
+        <h2 class="repository-name">${data.name}</h2>
+        <p class="repository-language">${data.language}</p>
+        <a href="${data.html_url}" class="repository-link" target="_blank">Ссылка на репозиторий</a>
+        </div>`;
+    list.insertAdjacentHTML("beforeend", HTML);
+}
